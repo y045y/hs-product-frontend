b4beb5000f858d64199d5ab8ca26affba1f4bb5d
y045y <y045y@msn.com>
2024-12-11 12:04:44 +0900

    印刷の追加

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
+import { useReactToPrint } from 'react-to-print';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
-
+  const printRef = useRef();
 
   const BASE_URL = process.env.REACT_APP_API_URL;
 
@@ -36,6 +37,13 @@ const ProductList = () => {
       });
   };
 
+  // 印刷機能
+  const handlePrint = useReactToPrint({
+    content: () => printRef.current,
+    documentTitle: '製品一覧',
+    onAfterPrint: () => console.log('印刷完了'),
+  });
+
   // エラーメッセージの表示
   if (error) {
     return <div className="alert alert-danger">{error}</div>;
@@ -50,26 +58,31 @@ const ProductList = () => {
   const sortedProducts = [...products].sort((a, b) => a.ProductId - b.ProductId);
 
   return (
-    <div className="card shadow p-4">
-      <h4 className="text-center text-info mb-4">製品一覧</h4>
-      <ul className="list-group">
-        {sortedProducts.map((product) => (
-          <li
-            key={product.ProductId}
-            className="list-group-item d-flex justify-content-between align-items-center"
-          >
-            <span>
-              {product.ProductName} - ¥{Number(product.Price).toLocaleString('ja-JP')}
-            </span>
-            <button
-              onClick={() => deleteProduct(product.ProductId)}
-              className="btn btn-danger btn-sm"
+    <div>
+      <div className="card shadow p-4" ref={printRef}>
+        <h4 className="text-center text-info mb-4">製品一覧</h4>
+        <ul className="list-group">
+          {sortedProducts.map((product) => (
+            <li
+              key={product.ProductId}
+              className="list-group-item d-flex justify-content-between align-items-center"
             >
-              削除
-            </button>
-          </li>
-        ))}
-      </ul>
+              <span>
+                {product.ProductName} - ¥{Number(product.Price).toLocaleString('ja-JP')}
+              </span>
+              <button
+                onClick={() => deleteProduct(product.ProductId)}
+                className="btn btn-danger btn-sm"
+              >
+                削除
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button className="btn btn-primary mt-4" onClick={handlePrint}>
+        印刷
+      </button>
     </div>
   );
 };
